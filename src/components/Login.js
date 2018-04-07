abd727d8877811abb75d5939362c860523234d5b
SamiJG <samantha.gildart@gmail.com>
2018-04-07 10:37:50 +0100

    login component

---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class Login extends Component {
+  render() {
+    return <div className="login col-md-3">Login</div>;
+  }
+}
+
+export default Login;
