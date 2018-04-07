3e5b8efae7e3e0482e2bde4fb4d75da2c73181d4
SamiJG <samantha.gildart@gmail.com>
2018-04-07 10:36:50 +0100

    filterbar for funtionality later

---
+++ src/components/FilterBar.js
@@ -0,0 +1,43 @@
+// import React, { Component } from 'react';
+// // import './App.css';
+
+// class FilterBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//         <form className="form-inline">
+//           <ul class="navbar-nav mr-auto">
+//             <li class="nav-item">
+//               <a class="nav-link" href="#">
+//                 Popular
+//               </a>
+//             </li>
+//             <li class="nav-item">
+//               <a class="nav-link" href="#">
+//                 Newest
+//               </a>
+//             </li>
+//             <li class="nav-item">
+//               <a class="nav-link" href="#">
+//                 Random
+//               </a>
+//             </li>
+//           </ul>
+//           <input
+//             className="form-control mr-sm-2"
+//             type="text"
+//             placeholder="Search"
+//           />
+//           <button
+//             className="btn btn-outline-success my-2 my-sm-0"
+//             type="submit"
+//           >
+//             Search
+//           </button>
+//         </form>
+//       </nav>
+//     );
+//   }
+// }
+
+// export default FilterBar;
